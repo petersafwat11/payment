0acf460aa9994456a5bf53e58bf5defcdd7d68cd
peter safwat <petersafwat640@gmail.com>
2024-11-15 07:56:00 +0200

    fix

--- app/page.js
+++ app/page.js
@@ -4,25 +4,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const ref = useRef(null);
-  // useEffect(() => {
-  //   const iframe = ref.current;
-  //   iframe.onload = () => {
-  //     try {
-  //       const iframeDocument =
-  //         iframe.contentDocument || iframe.contentWindow.document;
-  //       const targetP = Array.from(
-  //         iframeDocument.querySelectorAll("p.text")
-  //       ).find((p) => p.textContent === "Secure payment by Square");
-  //       if (targetP) {
-  //         // Do something with the targeted <p> element
-  //         targetP.style.display = "none"; // Example action
-  //       }
-  //     } catch (error) {
-  //       console.error("Cannot access iframe content:", error);
-  //     }
-  //   };
-  // }, []);
-
   return (
     <div className={styles.page}>
       <p className={styles["span"]}></p>
